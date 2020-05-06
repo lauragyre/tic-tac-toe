3fc2aa0e7d3bc710e0d37e5fcb3fb5807a402798
Laura Gyre <laura@trainingwingsworkshop.com>
2020-05-06 10:59:14 -0400

    plays and reset button enabled

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
-const squares = document.querySelectorAll('.square');
 
-const gameBoard = ((squareStatus) => {
-    for (let i = 0; i < 9; i++) {
+ const gameBoard = (() => {
+    let reset = document.querySelector('#reset');
+    const squares = document.querySelectorAll('.square');
+    let player = 'x';
+    reset.addEventListener('click', function (){
+        for (let i=0; i < squares.length; i++) {
+            gameBrain.makePlay(squares[i].id, null, true)
+        }
+    })
+    for (let i=0; i < squares.length; i++) {
+        squares[i].addEventListener('click', (event)=>{
+          gameBrain.makePlay(event.target.id, player, false);
+        })
+       }
+    const update = function (squareStatus){
+        console.log('updating');
+        console.log(squareStatus);
+       for (let i = 0; i < 9; i++) {
+        console.log("this" + squares[i]);
         squares[i].innerHTML = squareStatus[i];
-        
+       
+    }}
+    const playerToggle = function () {
+        player = (player == 'x')? 'o' : 'x';
     }
-  });
+    
+
+    return {update, playerToggle}
+
+  })();
+
 
   const gameBrain = (function () {
    boardValue = {
-    s1: 'x',
-    s2: 'o',
-    s3: 'x',
-    s4: 'x',
-    s5: 'o',
-    s6: 'o',
-    s7: 'o',
-    s8: 'x',
-    s9: 'x'
+    s1: null,
+    s2: null,
+    s3: null,
+    s4: null,
+    s5: null,
+    s6: null,
+    s7: null,
+    s8: null,
+    s9: null
    }
 
-   const makePlay = function (square, mark){
-   
+   const makePlay = function (square, mark, reset){
+        if (!boardValue[square] || reset) {
         boardValue[square] = mark;
+        gameBoard.playerToggle();
+        } else {alert('choose a blank square');}
         console.log(Object.values(boardValue));
-        gameBoard(Object.values(boardValue)); 
+        gameBoard.update(Object.values(boardValue)); 
    }
-   return { makePlay }
+
+   const evaluate = function () {
+       const banner = document.querySelector('#winner');
+       text = document.createTextNode('3 in a row, X wins!');
+       banner.append(text);
+   }
+
+   return { makePlay, evaluate }
   })();
 
-  function Player(firstName) {
-    const person = {};
-    person.firstName = firstName;
-    return person;
-  }
+  gameBrain.makePlay('s1', 'x', false);
+  gameBrain.makePlay('s2', 'o', false);
+  gameBrain.evaluate();
+ 
 
-  
- gameBrain.makePlay('s1', 'z');
- gameBrain.makePlay('s2', 'n');
 
- for (let i=0; i < squares.length; i++) {
- squares[i].addEventListener('click', (event)=>{
-   event.target.style.backgroundColor = 'green';
- })
-}
+ 
