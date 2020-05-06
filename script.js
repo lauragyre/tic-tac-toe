1f368d62b17a4380c36975c9ee83f0cc37e225e5
Laura Gyre <laura@trainingwingsworkshop.com>
2020-05-06 12:39:53 -0400

    finished

--- script.js
+++ script.js
@@ -2,31 +2,31 @@
  const gameBoard = (() => {
     let reset = document.querySelector('#reset');
     const squares = document.querySelectorAll('.square');
-    let player = 'x';
+    const bannerRef = document.querySelector('#winner');
     reset.addEventListener('click', function (){
         for (let i=0; i < squares.length; i++) {
             gameBrain.makePlay(squares[i].id, null, true)
         }
+        while (bannerRef.hasChildNodes()) {
+        bannerRef.removeChild(bannerRef.firstChild);
+        }
     })
     for (let i=0; i < squares.length; i++) {
         squares[i].addEventListener('click', (event)=>{
-          gameBrain.makePlay(event.target.id, player, false);
+          gameBrain.makePlay(event.target.id, 'x', false, false);
+          gameBrain.makePlay('s'+(Math.floor(Math.random())*9+1), 'o', false, true);
+          gameBrain.evaluate();
         })
        }
     const update = function (squareStatus){
-        console.log('updating');
-        console.log(squareStatus);
+       
        for (let i = 0; i < 9; i++) {
-        console.log("this" + squares[i]);
         squares[i].innerHTML = squareStatus[i];
        
     }}
-    const playerToggle = function () {
-        player = (player == 'x')? 'o' : 'x';
-    }
     
 
-    return {update, playerToggle}
+    return {update}
 
   })();
 
@@ -44,27 +44,63 @@
     s9: null
    }
 
-   const makePlay = function (square, mark, reset){
+   const makePlay = function (square, mark, reset, computer){
         if (!boardValue[square] || reset) {
         boardValue[square] = mark;
-        gameBoard.playerToggle();
-        } else {alert('choose a blank square');}
-        console.log(Object.values(boardValue));
+        }
+        else if (computer == true) {
+            while (boardValue[square] && Object.values(boardValue).includes(null)) {
+                square = 's'+(Math.floor(Math.random()*9) + 1);
+            }
+            boardValue[square] = mark;
+        }
+        else {alert('choose a blank square');}
+
         gameBoard.update(Object.values(boardValue)); 
    }
 
+   
+  
+
    const evaluate = function () {
-       const banner = document.querySelector('#winner');
-       text = document.createTextNode('3 in a row, X wins!');
-       banner.append(text);
+      let winner;
+      draw = document.createTextNode("It's a draw, try again.");
+      const banner = document.querySelector('#winner');
+      if (
+         boardValue['s1'] == 'x' && boardValue['s2'] == 'x' && boardValue['s3'] == 'x' ||
+         boardValue['s4'] == 'x' && boardValue['s5'] == 'x' && boardValue['s6'] == 'x' ||
+         boardValue['s7'] == 'x' && boardValue['s8'] == 'x' && boardValue['s9'] == 'x' ||
+         boardValue['s1'] == 'x' && boardValue['s4'] == 'x' && boardValue['s7'] == 'x' ||
+         boardValue['s2'] == 'x' && boardValue['s5'] == 'x' && boardValue['s8'] == 'x' ||
+         boardValue['s3'] == 'x' && boardValue['s6'] == 'x' && boardValue['s9'] == 'x' ||
+         boardValue['s1'] == 'x' && boardValue['s5'] == 'x' && boardValue['s9'] == 'x' ||
+         boardValue['s3'] == 'x' && boardValue['s5'] == 'x' && boardValue['s7'] == 'x'  
+           ) {
+        winner = "you win!";   
+        announce = document.createTextNode(`3 in a row, ${winner}`);
+        banner.append(announce);}
+        else if (
+            boardValue['s1'] == 'o' && boardValue['s2'] == 'o' && boardValue['s3'] == 'o' ||
+            boardValue['s4'] == 'o' && boardValue['s5'] == 'o' && boardValue['s6'] == 'o' ||
+            boardValue['s7'] == 'o' && boardValue['s8'] == 'o' && boardValue['s9'] == 'o' ||
+            boardValue['s1'] == 'o' && boardValue['s4'] == 'o' && boardValue['s7'] == 'o' ||
+            boardValue['s2'] == 'o' && boardValue['s5'] == 'o' && boardValue['s8'] == 'o' ||
+            boardValue['s3'] == 'o' && boardValue['s6'] == 'o' && boardValue['s9'] == 'o' ||
+            boardValue['s1'] == 'o' && boardValue['s5'] == 'o' && boardValue['s9'] == 'o' ||
+            boardValue['s3'] == 'o' && boardValue['s5'] == 'o' && boardValue['s7'] == 'o'
+        ) {
+            winner = "computer wins!";
+            announce = document.createTextNode(`3 in a row, ${winner}`);
+            banner.append(announce);}
+    else if (!Object.values(boardValue).includes(null)) {banner.append(draw);}
+       
    }
 
    return { makePlay, evaluate }
   })();
 
-  gameBrain.makePlay('s1', 'x', false);
-  gameBrain.makePlay('s2', 'o', false);
-  gameBrain.evaluate();
+  gameBrain.makePlay('s1', 'x', false, true);
+  gameBrain.makePlay('s2', 'o', false, true);
  
 
 
