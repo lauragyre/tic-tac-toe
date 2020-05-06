3d1adeb031b33908617360b05812ac399aadff19
Laura Gyre <laura@trainingwingsworkshop.com>
2020-05-06 09:03:55 -0400

    board and brain logic

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-
-console.log('running');
+const squares = document.querySelectorAll('.square');
 
 const gameBoard = ((squareStatus) => {
-    const squares = document.querySelectorAll('.square');
     for (let i = 0; i < 9; i++) {
         squares[i].innerHTML = squareStatus[i];
         
     }
   });
 
-  const gameBrain = function (square, mark) {
-   const s1 = "x";
-   const s2 = "x";
-   const s3 = "o";
-   const s4 = "x";
-   const s5 = "o";
-   const s6 = "o";
-   const s7 = "o";
-   const s8 = "x";
-   const s9 = "x";
-        square = mark;
-        gameBoard([s1, s2, s3, s4, s5, s6, s7, s8, s9]); 
-        
-  }
+  const gameBrain = (function () {
+   boardValue = {
+    s1: 'x',
+    s2: 'o',
+    s3: 'x',
+    s4: 'x',
+    s5: 'o',
+    s6: 'o',
+    s7: 'o',
+    s8: 'x',
+    s9: 'x'
+   }
+
+   const makePlay = function (square, mark){
+   
+        boardValue[square] = mark;
+        console.log(Object.values(boardValue));
+        gameBoard(Object.values(boardValue)); 
+   }
+   return { makePlay }
+  })();
 
   function Player(firstName) {
     const person = {};
@@ -30,6 +35,12 @@ const gameBoard = ((squareStatus) => {
     return person;
   }
 
- 
-  gameBrain('s1', 'z');
   
+ gameBrain.makePlay('s1', 'z');
+ gameBrain.makePlay('s2', 'n');
+
+ for (let i=0; i < squares.length; i++) {
+ squares[i].addEventListener('click', (event)=>{
+   event.target.style.backgroundColor = 'green';
+ })
+}
